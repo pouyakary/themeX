e2fe3c10d6347aaafa0f1d890b4757fb9d9b9691
Pouya Kary <k@karyfoundation.org>
2016-07-27 22:35:31 +0430

    buildcore init

---
+++ src/buildcore.ts
@@ -0,0 +1,32 @@
+
+//
+// Theme - A general color scheme theme generator
+//  Copyright 2016 Kary Foundation, Inc. All Rights Reserved.
+//  Authored by Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    import check = require('./checkcore');
+    import gi    = require('./interfaces');
+
+//
+// ─── BUILD ──────────────────────────────────────────────────────────────────────
+//
+
+    export = ( project: gi.bundle.base ): boolean => {
+        if ( !check( project ) ) return false;
+        applyBuild( project );
+    }
+
+//
+// ─── APPLY BUILD TO PROJECT ─────────────────────────────────────────────────────
+//
+
+    function applyBuild( project: gi.bundle.base ) {
+
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
