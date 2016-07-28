336e1c2248380c91414d1aed1c73f58af022c33f
Pouya Kary <k@karyfoundation.org>
2016-07-29 00:16:42 +0430

    buildcore complete

--- src/buildcore.ts
+++ src/buildcore.ts
@@ -9,14 +9,29 @@
 // ─── IMPORTS ────────────────────────────────────────────────────────────────────
 //
 
-    import check = require('./checkcore');
-    import gi    = require('./interfaces');
+    import themeX   = require('./themeX');
+    import check    = require('./checkcore');
+    import fs       = require('fs');
+    import path     = require('path');
+
+//
+// ─── INTERFACES ─────────────────────────────────────────────────────────────────
+//
+
+    interface IAdaptor {
+        id: string;
+        generate ( project: themeX.IBundle.base );
+        editor: string;
+        version: string;
+        name?: string;
+        author?: string;
+    }
 
 //
 // ─── BUILD ──────────────────────────────────────────────────────────────────────
 //
 
-    export = ( project: gi.bundle.base ): boolean => {
+    export = ( project: themeX.IBundle.base ): boolean => {
         if ( !check( project ) ) return false;
         applyBuild( project );
     }
@@ -25,8 +40,31 @@
 // ─── APPLY BUILD TO PROJECT ─────────────────────────────────────────────────────
 //
 
-    function applyBuild( project: gi.bundle.base ) {
-        
+    function applyBuild( project: themeX.IBundle.base ) {
+        let adaptorDirectory = getAdaptorDirectoryLocation( );
+        fs.readdir( adaptorDirectory, ( err , files ) => {
+            files.forEach( subDirectory => {
+                if ( /.adaptorX$/.test( subDirectory ) ) {
+                    var adaptor = <IAdaptor> require( 
+                        path.join( adaptorDirectory , subDirectory ) 
+                    );
+                    try {
+                        console.log(`----> running adaptor version ${ adaptor.version } for ${ adaptor.editor }`);
+                        adaptor.generate( project );
+                    } catch ( error ) {
+                        themeX.report( 2 , `-> themeX error: Could not generate theme for ${ adaptor.editor }.\n   editor: ${ adaptor.id }` );
+                    }
+                }
+            })
+        });
+    }
+
+//
+// ─── GET ALL THE ADAPTORS ───────────────────────────────────────────────────────
+//
+
+    function getAdaptorDirectoryLocation ( ): string {
+        return path.join( __dirname , 'adaptors' );
     }
 
 // ────────────────────────────────────────────────────────────────────────────────
